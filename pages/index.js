32a93b45a8968387f96b0a0b0b38efabb61f626c
Hanna Chen <hanna.chen@shopify.com>
2021-03-25 15:55:09 -0400

    Add empty state and card to display selected products

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,62 @@
-import { Heading, Page } from "@shopify/polaris";
+import React, { useState, useCallback, useEffect } from "react";
+import { EmptyState, Layout, Page, Card } from "@shopify/polaris";
+import { ResourcePicker } from "@shopify/app-bridge-react";
 
-const Index = () => (
-  <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
-  </Page>
-);
+const img = "https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg";
+
+const Index = () => {
+  const [isProductPickerOpen, setProductPickerOpen] = useState(false);
+  const [selectedProducts, setSelectedProducts] = useState([]);
+
+  const handleOpenProductPicker = useCallback(
+    (openState) => () => setProductPickerOpen(openState),
+    [isProductPickerOpen]
+  );
+  const handleSelectProducts = useCallback(
+    (selectionPayload) => setSelectedProducts(selectionPayload.selection),
+    [selectedProducts]
+  );
+
+  const emptyState = (
+    <>
+      <EmptyState
+        heading="Discount your products temporarily"
+        action={{
+          content: "Select products",
+          onAction: handleOpenProductPicker(true),
+        }}
+        image={img}
+      >
+        <p>Select products to change their price temporarily.</p>
+      </EmptyState>
+
+      <ResourcePicker
+        resourceType="Product"
+        open={isProductPickerOpen}
+        onSelection={handleSelectProducts}
+        onCancel={handleOpenProductPicker(false)}
+      />
+    </>
+  );
+
+  const productList = (
+    <Layout.AnnotatedSection
+      title="Default discount"
+      description="Add a product to Sample App, it will automatically be discounted."
+    >
+      <Card sectioned>
+        {selectedProducts.map((product, i) => (
+          <div key={i}>{product.title}</div>
+        ))}
+      </Card>
+    </Layout.AnnotatedSection>
+  );
+
+  return (
+    <Page>
+      <Layout>{selectedProducts.length ? productList : emptyState}</Layout>
+    </Page>
+  );
+};
 
 export default Index;
